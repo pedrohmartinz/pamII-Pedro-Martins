fc97b3b29a6e45991932fb87f3e46a2b2f2dfbe4
pedrohmartinz <pedrohmartinso@gmail.com>
2023-03-08 08:37:53 -0300

    Aula sobre javascript com variaveis e funcoes

---
+++ aula02/funcoes.js
@@ -0,0 +1,12 @@
+let num1 = 2;
+let num2 = 4;
+
+function soma(){
+    return num1 +num2
+}
+console.log(soma(num1,num2));
+
+const funcaoSoma = (a,b) =>{
+    return a+b;
+}
+console.log(funcaoSoma(10,6));
---
+++ aula02/funcoesevariaveis.js
@@ -0,0 +1,12 @@
+var a = "olá";
+let b = 1;
+const c = 6;
+
+console.log(a, "voce");
+
+const obj ={
+    nome:"Pedro",
+    signo:"Sigma"
+}
+console.log(obj.nome, obj.signo);
+console.log(typeof obj);
